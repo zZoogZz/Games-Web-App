b291c6ae63e983c588299c5b0ebacb809897b3df
William <19891289+zZoogZz@users.noreply.github.com>
2023-08-15 10:57:41 +1200

    Added game_id param to blueprint

--- games/gamedesc/gamedesc.py
+++ games/gamedesc/gamedesc.py
@@ -6,8 +6,9 @@ gamedesc_blueprint = Blueprint(
 
 # TODO: Access to the games should be implemented via the repository pattern, so this can not stay here!
 
-def create_some_game():
-    some_game = Game(1, "Call of Duty® 4: Modern Warfare®")
+
+def create_some_game(game_id):
+    some_game = Game(game_id, "Call of Duty® 4: Modern Warfare®")
     some_game.release_date = "Nov 12, 2007"
     some_game.price = 9.99
     some_game.description = "The new action-thriller from the award-winning team at Infinity Ward, the creators of " \
@@ -15,8 +16,11 @@ def create_some_game():
     some_game.image_url = "https://cdn.akamai.steamstatic.com/steam/apps/7940/header.jpg?t=1646762118"
     return some_game
 
-@gamedesc_blueprint.route('/desc/')
-def desc():
-    some_game = create_some_game()
+# TODO: This is a blueprint - this needs to render for each game.
+
+
+@gamedesc_blueprint.route('/game/<int:game_id>')
+def desc(game_id):
+    some_game = create_some_game(game_id)
     # Use Jinja to customize a predefined html page rendering the layout for showing a single game.
     return render_template('gameDescription.html', game=some_game)
