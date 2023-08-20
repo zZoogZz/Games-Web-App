72cc89686b7361130446cc2138c6203e8a9999c4
mcon754 <michael@connaughton.com.au>
2023-08-20 16:26:14 +1200

    Revised game.csv loading to match lecture 12 since it turns out we already had code for this (I've added image and web url to the loading)

--- games/adapters/datareader/csvdatareader.py
+++ games/adapters/datareader/csvdatareader.py
@@ -25,6 +25,8 @@ class GameFileCSVReader:
                     game.release_date = row["Release date"]
                     game.price = float(row["Price"])
                     game.description = row["About the game"]
+                    game.image_url = row["Website"]
+                    game.website_url = row["Header image"]
 
                     publisher = Publisher(row["Publishers"])
                     self.__dataset_of_publishers.add(publisher)
