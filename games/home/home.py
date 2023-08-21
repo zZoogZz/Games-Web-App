36aa9b3d20159876d261ae8de2f996c5b31b97a6
William <19891289+zZoogZz@users.noreply.github.com>
2023-08-21 21:03:34 +1200

    Enable Search Form

--- games/home/home.py
+++ games/home/home.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+import flask
+from flask import Blueprint, render_template, request, url_for
 
 import games.utilities.utilities as utilities
 
@@ -8,5 +9,12 @@ home_blueprint = Blueprint(
 
 @home_blueprint.route('/', methods=['GET'])
 def home():
-    return render_template('home/home.html',
-                           top_genres=utilities.get_top_genres())
+    return render_template('home/home.html', top_genres=utilities.get_top_genres())
+
+
+@home_blueprint.route('/', methods=['POST'])
+def home_search_post():
+    query = request.form['query']
+    query_type = "title"
+
+    return flask.redirect(url_for("search_bp.search_games", query=query, query_type=query_type))
