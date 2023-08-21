de0bd64cb5df4d41afa96e5a8ac7af8fc923c10c
William <19891289+zZoogZz@users.noreply.github.com>
2023-08-21 18:35:52 +1200

    Split all games into PC, Games List and Games Template

--- games/allGames/allGames.py
+++ games/allGames/allGames.py
@@ -9,15 +9,17 @@ import games.allGames.services as services
 all_games_blueprint = Blueprint(
     'all_games_bp', __name__)
 
+
 def all_games_by_name():
     # Returns list of all game objects from CSV file in alphabetical order
     all_games = services.all_games_by_name(repo.repo_instance)
     return all_games
 
+
 @all_games_blueprint.route('/all_games', methods=['GET'])
 def all_games():
-        all_games = all_games_by_name()
-        # Use Jinja to customize a predefined html page rendering the layout for all games in alphabetical order
-        return render_template('allGames.html', all_games=all_games, top_genres=utilities.get_top_genres())
+    all_games = all_games_by_name()
+    # Use Jinja to customize a predefined html page rendering the layout for all games in alphabetical order
+    return render_template('games/games.html', all_games=all_games, top_genres=utilities.get_top_genres())
 
 
