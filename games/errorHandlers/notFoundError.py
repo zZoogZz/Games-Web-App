10046d276844dee6709ed49371d261238509bb3e
Simon <simon.gadd777@gmail.com>
2023-08-23 12:18:40 +1200

    Fixed blueprint register stuff

--- games/errorHandlers/notFoundError.py
+++ games/errorHandlers/notFoundError.py
@@ -4,6 +4,6 @@ not_found_blueprint = Blueprint(
     'errorhandler_bp', __name__)
 
 
-@not_found_blueprint.errorhandler(404)
+@not_found_blueprint.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
