103f7d64a4c92206f184417f4a845874caa13e16
mcon754 <michael@connaughton.com.au>
2023-08-29 01:20:50 +1200

    WIP - layout structure sort of ok but search not working.

--- games/errorHandlers/notFoundError.py
+++ games/errorHandlers/notFoundError.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint
+from games.utilities import utilities
 
 not_found_blueprint = Blueprint(
     'errorhandler_bp', __name__)
@@ -6,4 +7,4 @@ not_found_blueprint = Blueprint(
 
 @not_found_blueprint.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('errors/404.html'), 404
+    return render_template('errors/404.html', top_genres=utilities.get_top_genres()), 404
