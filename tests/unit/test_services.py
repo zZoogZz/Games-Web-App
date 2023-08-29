2be6f48a884e555625498ef3a5a8d4b4fc9aa3a8
Simon <simon.gadd777@gmail.com>
2023-08-30 00:36:38 +1200

    Added some test functions for the services in allGames

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -87,7 +87,10 @@ def test_query_genres():
             if search_term.lower() in genre:
                 counter += 1
         assert counter > 0
-
+    searched_genres1 = allGamesServices.query_genre("", repo.repo_instance)
+    assert len(searched_genres1) == len(allGames)  # defaults to all games
+    searched_genres2 = allGamesServices.query_genre("123456;;;;;", repo.repo_instance)
+    assert len(searched_genres2) == 0
 
 
 
