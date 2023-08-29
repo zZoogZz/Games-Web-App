63e3df19045c2c24fe8e628278d9dd3db78dfcdb
mcon754 <michael@connaughton.com.au>
2023-08-29 22:51:27 +1200

    No results exception and page appear to work.

--- games/allGames/services.py
+++ games/allGames/services.py
@@ -1,6 +1,6 @@
 from games.adapters.repository import AbstractRepository
 import games.adapters.repository as repository
-
+from games.errorHandlers.custom_exceptions import NoResultsFoundException
 
 def query_all_games_by_name(repo=repository.repo_instance):
     game_ids_sorted = repo.get_game_ids_sorted_by_title()
@@ -21,7 +21,8 @@ def query_games_title(query, repo=repository.repo_instance):
     for game in sorted_game_object_list:
         if query.lower() in game.title.lower():
             result.append(game)
-
+    if result == []:
+        raise NoResultsFoundException(f"No games found for {query} in titles.")
     return result
 
 
@@ -38,7 +39,8 @@ def query_publisher(query, repo=repository.repo_instance):
     for game in sorted_game_object_list:
         if query.lower() in game.publisher.publisher_name.lower():
             result.append(game)
-
+    if result == []:
+        raise NoResultsFoundException(f"No games found for {query} in publishers.")
     return result
 
 
@@ -52,5 +54,6 @@ def query_genre(query, repo=repository.repo_instance):
         for genre in game.genres:
             if query.lower() in genre.genre_name.lower() and game not in result:
                 result.append(game)
-
+    if result == []:
+        raise NoResultsFoundException(f"No games found for {query} in genres.")
     return result
