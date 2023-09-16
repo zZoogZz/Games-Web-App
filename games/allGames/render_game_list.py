47c9537a25572ea71cb756d0ac7cbc48ef5abb46
William <19891289+zZoogZz@users.noreply.github.com>
2023-09-16 12:24:19 +1200

    Added 404 Handler

--- games/allGames/render_game_list.py
+++ games/allGames/render_game_list.py
@@ -1,21 +1,27 @@
 from games.utilities import utilities
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, abort
 import games.allGames.services as services
-#
-# TODO IMPLEMENT PAGINATION
-#
-def render_game_list(game_list):
+
+GAMES_PER_PAGE = 20
+
+
+def render_game_list(game_list, heading="List name not found."):
     """
     Takes a list of games and paginates then renders the output.
     """
-    page = request.args.get('page', 1, type=int)
-    games_per_page = 20
-    search_query = request.args.get('query')
-    search_query_type = request.args.get('query_type')
-    start_index = (page - 1) * games_per_page
-    end_index = start_index + games_per_page
+
+    if len(game_list) < 1: abort(404) # 404 Redirect if no game found.
+
+    page = request.args.get('page', 1, type=int) #Gets page number
+    
+    # search_query = request.args.get('query')
+    # search_query_type = request.args.get('query_type')
+    
+    start_index = (page - 1) * GAMES_PER_PAGE
+    end_index = start_index + GAMES_PER_PAGE
     paginated_games = game_list[start_index:end_index]
     total_games = len(game_list)
-    total_game_pages = (total_games + games_per_page - 1) // games_per_page
+    total_game_pages = (total_games + GAMES_PER_PAGE - 1) // GAMES_PER_PAGE
+
     return render_template('games/games.html', all_games=paginated_games, top_genres=utilities.get_top_genres(),
-                           page=page, total_game_pages=total_game_pages, search_query=search_query, search_query_type=search_query_type)
+                           page=page, total_game_pages=total_game_pages, heading=heading)
