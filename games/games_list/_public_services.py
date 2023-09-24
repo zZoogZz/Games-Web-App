b9e11e2074cc0fb602f12ef8d53f4cfa2c35c2ef
William <19891289+zZoogZz@users.noreply.github.com>
2023-09-24 18:16:24 +1300

    Resolved weird behaviour in tests

--- games/games_list/_public_services.py
+++ games/games_list/_public_services.py
@@ -1,14 +1,14 @@
 from flask import session
-import games.adapters.repository as repository
+import games.adapters.repository as repo
 from games.authentication.authentication import login_required
 
 
 @login_required
-def toggle_favourite(game, repo=repository.repo_instance):
+def toggle_favourite(game, repo=repo):
     """
     Toggles the games status as a favourite.
     """
-    user = repo.get_user(session['user_name'])
+    user = repo.repo_instance.get_user(session['user_name'])
 
     if game in user.favourite_games:
         user.remove_favourite_game(game)
@@ -16,15 +16,15 @@ def toggle_favourite(game, repo=repository.repo_instance):
         user.add_favourite_game(game)
 
 
-def toggle_wishlist(game, repo=repository.repo_instance):
+def toggle_wishlist(game, repo):
     """
     Toggles the games status on a wishlist.
     """
     raise NotImplementedError
 
 
-def is_favourite(game, repo=repository.repo_instance):
-    user = repo.get_user(session['user_name'])
+def is_favourite(game, repo=repo):
+    user = repo.repo_instance.get_user(session['user_name'])
     if user is None:
         return False
 
@@ -34,11 +34,11 @@ def is_favourite(game, repo=repository.repo_instance):
         return False
 
 
-def get_favourites(repo=repository.repo_instance):
+def get_favourites(repo=repo):
     if 'user_name' not in session:
         return []
 
-    user = repo.get_user(session['user_name'])
+    user = repo.repo_instance.get_user(session['user_name'])
 
     if user is None:
         return []
