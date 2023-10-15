f1087c698ad769177a7b9307b65b46442b9d9482
William <19891289+zZoogZz@users.noreply.github.com>
2023-10-15 22:41:39 +1300

    Added Favourites Test

--- tests_db/unit/test_database_repository.py
+++ tests_db/unit/test_database_repository.py
@@ -73,20 +73,52 @@ def test_repository_can_get_games_by_game_ids(session_factory):
     all_games = test_repo.get_games_by_ids(all_game_ids)
     assert len(all_games) == 877
 
+# Checks that a user can add a review to a game.
 def test_repository_user_can_add_a_review(session_factory):
     test_repo = SqlAlchemyRepository(session_factory)
+    # Initialise
     user = User('User', 'Password!123')
     test_repo.add_user(user)
     game = Game(1, "Test Game")
     game.price = 69
     game.release_date = 'Oct 21, 2008'
     test_repo.add_game(game)
+
+    # Get Objects from Database
     retrieved_game = test_repo.get_game(1)
     retrieved_user = test_repo.get_user('user')
+
+    # Add a review, check the review was added.
     review = Review(retrieved_user, retrieved_game, 5, "Great game!")
     test_repo.add_review(review)
     assert review in test_repo.get_reviews()
 
 
+def test_repository_favorite_testing(session_factory):
+    test_repo = SqlAlchemyRepository(session_factory)
 
+    # Create User
+    user = User('User', 'Password!123')
+    test_repo.add_user(user)
+
+    # Create Game
+    game = Game(430, 'Hearts')
+    game.price = 69
+    game.release_date = 'Oct 21, 2008'
+    test_repo.add_game(game)
+
+    # Get User & Game
+    retrieved_user = test_repo.get_user('user')
+    retrieved_game = test_repo.get_game(430)
+
+    # Check the user does not have any favourites before any are set.
+    assert test_repo.get_favourites(retrieved_user) == []
+
+    # Add and Check the above game is in the favourites list by toggling it.
+    test_repo.toggle_favourite(retrieved_game, retrieved_user)
+    assert retrieved_game in test_repo.get_favourites(retrieved_user)
+
+    # Check toggling again removes the game from the list.
+    test_repo.toggle_favourite(retrieved_game, retrieved_user)
+    assert test_repo.get_favourites(retrieved_user) == []
 
